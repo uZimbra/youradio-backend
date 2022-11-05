d96cd9021f935147bd4b2ed0ea0afd7db44caf10
Rodrigo A. Silva <rgbrasil2@gmail.com>
2022-11-05 12:35:02 -0300

    chore:  music path adjustment

--- src/modules/music/repositories/local/StorageRepositoryLocal.ts
+++ src/modules/music/repositories/local/StorageRepositoryLocal.ts
@@ -1,7 +1,8 @@
 import { IUploadMusicDTO } from "@modules/music/dtos/IUploadMusicDTO";
-import { IStorageRepository } from "../IStorageRepository";
-import { Readable } from "stream";
 import { createReadStream } from "fs";
+import { join } from "path";
+import { Readable } from "stream";
+import { IStorageRepository } from "../IStorageRepository";
 
 class StorageRepositoryLocal implements IStorageRepository {
   async saveObject(data: IUploadMusicDTO): Promise<string> {
@@ -11,7 +12,7 @@ class StorageRepositoryLocal implements IStorageRepository {
   }
 
   getObject(key: string): Readable {
-    const musicPath = `/youradio/tmp/${key}`;
+    const musicPath = join(__dirname, "..", "..", "..", "..", "..", "tmp", key);
     return createReadStream(musicPath);
   }
 }
